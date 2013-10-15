d4dc9a2ffae827104054d23fc5196370d89493e0
Peter He <hexinpeter@gmail.com>
2013-10-15 17:09:15 +1100

    New Project

---
+++ COMP10002 Project2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  COMP10002 Project2
+//
+//  Created by He Xin on 15/10/13.
+//  Copyright (c) 2013 He Xin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
